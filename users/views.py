4f95f255fd8eb33d5289c181e8c65a74fa93d06c
vi5h4l <vishal.kr2003@gmail.com>
2025-01-20 15:00:12 +0530

    Login user and auth - Added Login functionality

--- users/views.py
+++ users/views.py
@@ -1,16 +1,31 @@
 from django.shortcuts import render,redirect
 
 # To use forms from django 
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login
 
 # Create your views here.
 def register_view(request):
     if request.method == "POST":
         form = UserCreationForm(request.POST)
         if form.is_valid():
-            form.save()
+            #form.save() saves user in admin and also gives the user which we are using to login
+            login(request,form.save())
             return redirect("posts:list")
-    else:
+    else:  #GET REQUEST 
         form = UserCreationForm()
 
     return render(request,"users/register.html",{'form':form})
+
+
+def login_view(request):
+    if request.method == "POST":
+        form = AuthenticationForm(data = request.POST)
+        if form.is_valid():
+            #LOGIN HERE
+            login(request, form.get_user())
+            return redirect("posts:list")       
+    else: #GET REQUEST 
+        form = AuthenticationForm()
+
+    return render(request,"users/login.html",{'form':form})
